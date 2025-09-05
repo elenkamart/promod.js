7aa044ad0210accb1342e0a6cc088eb70c119dd7
elenkamart <elena.beymart@hotmail.com>
2025-09-05 14:57:00 +0200

    add to cart update

--- tests/firsttest.spec.ts
+++ tests/firsttest.spec.ts
@@ -19,4 +19,15 @@ test ('Sign in', async({page}) => {
      await page.getByLabel('email address').press('Tab')
      await page.getByLabel('Password').fill('Welcome123')
      await page.getByRole('button', {name: 'LOG-IN'}).click()
+     expect(await page.waitForURL('https://www.promod.com/en/account/dashboard/'));
 });
+test ('Add to Cart', async ({page}) => {
+     await page.getByRole('button', {name: 'Ready to wear'}).click()
+     await page.locator('a[href="/en/suits--plblta4d53b87a9910b7b/"]').click();
+     expect(await page.waitForURL('https://www.promod.com/en/suits--plblta4d53b87a9910b7b/'));
+     let elementBlazer = await page.getByRole('button', {name: 'Buy Plain crossover blazer'});
+     await elementBlazer.scrollIntoViewIfNeeded();
+     await elementBlazer.click();
+     expect(await page.locator('#product-203445--name').textContent()).toEqual('Plain crossover blazer');
+
+})
