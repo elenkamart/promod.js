0acb6cfa52ae8183b5a7625b0142c6c9ddd894da
elenkamart <elena.beymart@hotmail.com>
2025-09-08 14:38:16 +0200

    update add to cart

--- tests/firsttest.spec.ts
+++ tests/firsttest.spec.ts
@@ -25,9 +25,14 @@ test ('Add to Cart', async ({page}) => {
      await page.getByRole('button', {name: 'Ready to wear'}).click()
      await page.locator('a[href="/en/suits--plblta4d53b87a9910b7b/"]').click();
      expect(await page.waitForURL('https://www.promod.com/en/suits--plblta4d53b87a9910b7b/'));
-     let elementBlazer = await page.getByRole('button', {name: 'Buy Plain crossover blazer'});
-     await elementBlazer.scrollIntoViewIfNeeded();
-     await elementBlazer.click();
-     expect(await page.locator('#product-203445--name').textContent()).toEqual('Plain crossover blazer');
-
+     const blazer = (await page.waitForSelector('#product-203445--name')).click();
+     await page.evaluate(() => window.scrollBy(0, 2000));
+     await page.waitForTimeout(5000);
+     expect(await page.waitForURL('https://www.promod.com/en/plain-crossover-blazer-ecru--p203445/'));
+     await page.locator('div[class="pmd-slidingLayer pmd-sizeSelector"]').click();
+     await page.locator('span[class="pmd-productSize_thumbnail"]').click();
+     
+     
+   
+  
 })
