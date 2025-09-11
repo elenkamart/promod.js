99cae48becc9fc4ed6d7fd7c0586a912d0584176
elenkamart <elena.beymart@hotmail.com>
2025-09-11 12:58:21 +0200

    UPDATE cart and new Order

--- tests/firsttest.spec.ts
+++ tests/firsttest.spec.ts
@@ -3,7 +3,7 @@ test.beforeEach(async({page}) => {
      await page.goto('https://www.promod.com/')
      await page.locator('#link_optoutAll').click()
 })
-test ('Create an account', async ({page}) => {
+test ('Create An Account', async ({page}) => {
      await page.getByRole('link', { name: 'Account' }).click();
      await page.getByTitle('Create an account').click();
      const createMyAccount = await page.locator('[class="pmd-form"]').filter({hasText: "CREATE MY ACCOUNT"})
@@ -12,7 +12,7 @@ test ('Create an account', async ({page}) => {
      await createMyAccount.getByRole('button', {name: 'CREATE MY ACCOUNT'}).click();
 
 });
-test ('Sign in', async({page}) => {
+test ('Sign In', async({page}) => {
      await page.getByRole('link', { name: 'Account' }).click()
      await page.getByLabel('email address').click()
      await page.getByLabel('email address').fill('elenapumpkin8@gmail.com')
@@ -21,7 +21,7 @@ test ('Sign in', async({page}) => {
      await page.getByRole('button', {name: 'LOG-IN'}).click()
      expect(await page.waitForURL('https://www.promod.com/en/account/dashboard/'));
 });
-test ('Add to Cart', async ({page}) => {
+test ('Add To Cart', async ({page}) => {
      await page.getByRole('button', {name: 'Ready to wear'}).click()
      await page.locator('a[href="/en/suits--plblta4d53b87a9910b7b/"]').click();
      expect(await page.waitForURL('https://www.promod.com/en/suits--plblta4d53b87a9910b7b/'));
@@ -31,12 +31,16 @@ test ('Add to Cart', async ({page}) => {
      expect(await page.waitForURL('https://www.promod.com/en/plain-crossover-blazer-ecru--p203445/'));
      await page.locator('div[class="pmd-slidingLayer pmd-sizeSelector"]').click();
      await page.getByRole('button', {name: '42'}).click();
-     await page.getByLabel('Add to shopping cart').click();
-     
+     expect(await page.waitForURL('https://www.promod.com/en/plain-crossover-blazer-ecru--p203445/?sku=1627087002142'));
+     await page.getByRole('button', {name:"ADD TO SHOPPING CART"}).first().click();
+     expect(await page.getByTitle('ITEM ADDED'));
+     await page.getByRole('button', {name:"SEE YOUR SHOPPING CART"}).first().click();
+     expect(await page.waitForURL('https://www.promod.com/en/cart/'));
+});
+test('Make An Order', async ({page}) => {
+     await page.goto('https://www.promod.com/en/cart/');
+     await page.getByText('LOG IN').click();
 
 
-     
-     
-   
-  
+
 })
